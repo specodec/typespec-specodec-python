44addd475f0f87b5ff0a1abf486ab94b67d10a28
yutingrui <yutingrui@tal>
2026-04-29 11:14:19 +0800

    feat: add SpecReader/SpecCodec + full codec generation

--- dist/src/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { EmitContext } from "@typespec/compiler";
-export type EmitterOptions = {
-    "emitter-output-dir": string;
-};
-export declare function $onEmit(context: EmitContext<EmitterOptions>): Promise<void>;
